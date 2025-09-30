f061cd58bb1c8fdf97a32c3624ec4dc22234fef6
Mohan Prasad <132048375+mohanswag@users.noreply.github.com>
2025-09-30 13:11:58 +0530

    LoginActivity.java

---
+++ LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication; // Replace with your package name
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // You'll add your login UI elements and logic here later
+    }
+}
